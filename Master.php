22df151893b4e7027d9addfd2d6e89af04c0b114
Sakshi-005 <58349414+Sakshi-005@users.noreply.github.com>
2019-11-29 22:19:55 -0500

    Add files via upload

---
+++ Master.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Nanum+Myeongjo&display=swap" rel="stylesheet">
+    <link href="Assets/Css/Style.css" rel="stylesheet" />
+</head>
+<body style="background-color: #f6f6f6">
+
+
